1e0dd26cc35059772947d153ea5beccc08fb2abf
dcchoi <dcchoi1234@gmail.com>
2017-12-27 20:36:03 -0500

    reversed linked list implement

--- ReverseLinkedList.js
+++ ReverseLinkedList.js
@@ -3,10 +3,32 @@ function LinkedListNode(value) {
     this.next = null;
 }
 
-function LinkedList(){
-    
+function ReverseLinkedList(headNode){
+    let pNode = null;
+    let cNode = headNode;
+    while(cNode.next){
+        let nNode = cNode.next;
+        cNode.next = pNode;
+        pNode = cNode;
+        cNode = nNode
+    }
+    //link up cNode with pNode;
+    cNode.next = pNode
+    return cNode;
 }
 
-let someObj = new LinkedListNode(10);
+function iterateLinkedList(headNode){
+    let cNode = headNode;
+    while(cNode){
+        console.log(cNode.value);
+        cNode = cNode.next;
+    }
+}
 
-console.log(someObj.value);
+let someObj1 = new LinkedListNode(10);
+let someObj2 = new LinkedListNode(11);
+let someObj3 = new LinkedListNode(12)
+someObj1.next = someObj2
+someObj2.next = someObj3;
+
+iterateLinkedList(ReverseLinkedList(someObj2));
